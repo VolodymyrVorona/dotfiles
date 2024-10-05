f32e335c2f800e6dd62c742159d49e0a87f884be
Volodymyr Vorona <volodymyr.voron87@gmail.com>
2024-10-05 14:01:45 +0300

    Add nvim config

---
+++ nvim/.config/nvim/lua/community.lua
@@ -0,0 +1,20 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.terminal-integration.vim-tmux-navigator" },
+  { import = "astrocommunity.search.grug-far-nvim" },
+
+  -- import/override with your plugins folder
+}
