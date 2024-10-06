43c67f55102c09109484a40085b27a4b1c8b6d9d
Volodymyr Vorona <53271437+VolodymyrVorona@users.noreply.github.com>
2024-10-06 17:37:41 +0300

    feat(yazi): Add the shortcat for yazi

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -9,3 +9,12 @@ lg()
             rm -f "$LAZYGIT_NEW_DIR_FILE" > /dev/null
     fi
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
